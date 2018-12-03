2b9af3f2bda7ba2de3d160c63e84396b2d79d629
Filippo Calzavara <filippo.calzavara@mail.polimi.it>
2018-12-03 19:32:26 +0100

    Update

---
+++ dataset/main.py
@@ -0,0 +1,10 @@
+class main:
+    def hello(self):
+        print "hello"
+    def hello2(self):
+        print "helo2"
+
+if __name__ == '__main__':
+    main = main()
+    main.hello()
+    main.hello2()
